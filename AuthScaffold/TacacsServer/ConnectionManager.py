a8d9e1cc57fc0e5d84b7024c4829ee3740a9fc49
Rodney Persky <rodney.persky@outlook.com>
2024-04-01 23:27:14 +1000

    connection manager doesn't need a session manager

--- AuthScaffold/TacacsServer/ConnectionManager.py
+++ AuthScaffold/TacacsServer/ConnectionManager.py
@@ -12,14 +12,9 @@ class ConnectionManager(IConnectionManager):
     def __init__(self) -> None:
         super().__init__()
 
-        self._session_manager : ISessionManager = ISessionManager()
-
     def OnConnectionStarted(self, client : IManagedConnection) -> Optional[IConnectionContext]:
         return None
 
     def OnConnectionClosed(self, context : IConnectionContext):
         pass
 
-    def HandlePacket(self, tacacsPacket : Packet, context : IConnectionContext):
-        self._session_manager.start_session(tacacsPacket, context)
-        pass
